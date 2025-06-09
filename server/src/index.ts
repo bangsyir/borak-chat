696e55c667d01339dba04e8f5b741344403fd102
bangsyir <bangsyir@gmail.com>
2025-06-09 19:33:37 +0800

    add rooms routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,14 +4,37 @@ import { authRoutes } from "./interfaces/http/routes/auth.route";
 import { prettyJSON } from "hono/pretty-json";
 import { friendshipRoutes } from "./interfaces/http/routes/friendship.route";
 import { messagesRoutes } from "./interfaces/http/routes/messages.route";
+import { roomsRoutes } from "./interfaces/http/routes/rooms.route";
+import { HTTPException } from "hono/http-exception";
 
 const app = new Hono();
+
+// global error
+app.onError((err, c) => {
+  if (err instanceof HTTPException) {
+    return c.json(
+      {
+        success: false,
+        message: err.message,
+      },
+      err.status,
+    );
+  }
+  return c.json(
+    {
+      success: false,
+      message: "Internal Server Error",
+    },
+    500,
+  );
+});
 
 app.use("*", prettyJSON());
 app.use(logger());
 app.route("/api/auth", authRoutes);
 app.route("/api", friendshipRoutes);
 app.route("/api", messagesRoutes);
+app.route("/api", roomsRoutes);
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
