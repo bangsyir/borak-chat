c5401333bffbaaf26fb81e4e9d9bb60f317cff81
bangsyir <bangsyir@gmail.com>
2025-06-14 21:20:08 +0800

    add sidebar

---
+++ client/app/components/sidebar/nav-user.tsx
@@ -0,0 +1,115 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "~/components/ui/dropdown-menu";
+import {
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  useSidebar,
+} from "../ui/sidebar";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { useFetcher, useNavigate } from "react-router";
+import React, { useRef } from "react";
+import { toast } from "sonner";
+import { ChevronsUpDown, LogOutIcon, UserCircleIcon } from "lucide-react";
+
+export function NavUser({
+  user,
+}: {
+  user: {
+    username: string;
+    email: string;
+    avatar: string;
+  };
+}) {
+  const { isMobile } = useSidebar();
+
+  const fetcher = useFetcher();
+  const navigate = useNavigate();
+  const hasDisplayToast = useRef(false);
+
+  React.useEffect(() => {
+    if (fetcher?.data?.success === false) {
+      toast.error("Opsss Something wrong with this apps");
+      hasDisplayToast.current = true;
+    }
+
+    if (fetcher?.data?.success === true) {
+      toast.success("You are logged out.", {
+        description: "See you soon. we waiting for you",
+      });
+      hasDisplayToast.current = true;
+      navigate("/login");
+    }
+  }, [fetcher.data]);
+
+  function handleLogout() {
+    fetcher.submit({}, { method: "post", action: "/logout" });
+  }
+  return (
+    <SidebarMenu>
+      <SidebarMenuItem>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <SidebarMenuButton
+              size="lg"
+              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground border"
+            >
+              <Avatar className="h-8 w-8 rounded-lg grayscale">
+                <AvatarImage src={user.avatar} alt={user.username} />
+                <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+              </Avatar>
+              <div className="grid flex-1 text-left text-sm leading-tight">
+                <span className="truncate font-medium">{user.username}</span>
+                <span className="text-muted-foreground truncate text-xs">
+                  {user.email}
+                </span>
+              </div>
+              <ChevronsUpDown className="ml-auto size-4" />
+            </SidebarMenuButton>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent
+            className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
+            side={isMobile ? "bottom" : "right"}
+            align="end"
+            sideOffset={4}
+          >
+            <DropdownMenuLabel className="p-0 font-normal">
+              <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                <Avatar className="h-8 w-8 rounded-lg">
+                  <AvatarImage src={user.avatar} alt={user.username} />
+                  <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                </Avatar>
+                <div className="grid flex-1 text-left text-sm leading-tight">
+                  <span className="truncate font-medium">{user.username}</span>
+                  <span className="text-muted-foreground truncate text-xs">
+                    {user.email}
+                  </span>
+                </div>
+              </div>
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuGroup>
+              <DropdownMenuItem>
+                <UserCircleIcon />
+                Account
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+            <DropdownMenuGroup>
+              <DropdownMenuItem variant="destructive" onClick={handleLogout}>
+                <LogOutIcon className="text-red-400" />
+                <button type="submit">Logout</button>
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </SidebarMenuItem>
+    </SidebarMenu>
+  );
+}
---
+++ client/app/routes/layout.tsx
@@ -0,0 +1,23 @@
+import type React from "react";
+import { Outlet } from "react-router";
+import { AppSidebar } from "~/components/sidebar/app-sidebar";
+import { SiteHeader } from "~/components/sidebar/site-header";
+import { SidebarInset, SidebarProvider } from "~/components/ui/sidebar";
+
+export default function Layout() {
+  return (
+    <SidebarProvider>
+      <AppSidebar variant="sidebar" />
+      <SidebarInset>
+        <SiteHeader />
+        <div className="flex flex-1 flex-col">
+          <div className="@container/main flex flex-1 flex-col gap-2">
+            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+              <Outlet />
+            </div>
+          </div>
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
