527efc9a473bbf66fc77dd5e9643b2af08221e06
bangsyir <bangsyir@gmail.com>
2025-07-04 23:09:09 +0800

    fix cookie handling

--- client/app/lib/session.server.ts
+++ client/app/lib/session.server.ts
@@ -1,4 +1,4 @@
-import { createCookieSessionStorage } from "react-router";
+import { createCookieSessionStorage, redirect } from "react-router";
 
 const SESSION_SECRET = import.meta.env.VITE_SESSION_SECRET as string;
 const { getSession, commitSession, destroySession } =
@@ -17,6 +17,27 @@ export { getSession, commitSession, destroySession };
 
 export async function getToken(request: Request) {
   const session = await getSession(request.headers.get("Cookie"));
-  const token = await session.get("__session").token;
+  const sessionData = await session.get("__session");
+  if (!sessionData) throw await logout(request);
+  const token = sessionData.token;
+  if (!token) throw await logout(request);
+  return { session, token };
+}
+
+export async function logout(request: Request) {
+  const session = await getSession(request.headers.get("Cookie"));
+  return redirect("/login", {
+    headers: {
+      "Set-Cookie": await destroySession(session),
+    },
+  });
+}
+
+export async function authUser(request: Request) {
+  const session = await getSession(request.headers.get("Cookie"));
+  const sessionData = await session.get("__session");
+  if (!sessionData) throw await logout(request);
+  const token = await sessionData.token;
+  if (!token) throw await logout(request);
   return { session, token };
 }
